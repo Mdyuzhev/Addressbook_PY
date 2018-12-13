0407f68117fedc99b041b9dc438c3b19e4d89534
Mdjugev <mdjugev@gmail.com>
2018-12-13 15:35:47 +0300

    прохождение тестов в одной сессии

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,4 +1,2 @@
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.deletion_first_group()
-    app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,12 +4,8 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="name_mod"))
-    app.session.logout()
 
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="header_mod"))
-    app.session.logout()
