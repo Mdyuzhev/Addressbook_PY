bec6ee048c409358b660a3f87e4e9963e2522d69
Mdjugev <mdjugev@gmail.com>
2018-12-13 12:56:11 +0300

    выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -8,30 +9,9 @@ class Application:
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
-    def return_to_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("group page").click()
 
-    def create_group(self, group):
-        driver = self.driver
-        self.open_groups_page()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        driver.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
 
     def open_home_page(self):
         driver = self.driver
